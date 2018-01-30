9d537e94097505540773b3d163048299121a181c
TOSHIBA <TOSHIBA@ANGEL>
2018-01-30 17:24:31 -0500

    Angel

--- src/comercial/rnegocio/entidades/Cliente.java
+++ src/comercial/rnegocio/entidades/Cliente.java
@@ -49,6 +49,9 @@ public class Cliente {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-    
+     @Override
+    public String toString() {
+        return nombre; 
+    }
     
 }
